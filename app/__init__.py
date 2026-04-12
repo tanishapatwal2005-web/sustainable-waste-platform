3866d43079e13066c0cfeae1273ed5585bc903b6
Tanisha Patwal <tanishapatwal2005@gmail.com>
2026-04-12 22:04:40 +0530

    fix template path correctly

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__, template_folder='app/templates')
+    app = Flask(__name__, template_folder='templates')
 
     @app.route('/')
     def home():
